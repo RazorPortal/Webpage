4877b5983ba9122577277704227bea3f78d7bf09
rnakamoto <rnakamot@uark.edu>
2015-04-02 23:08:10 -0500

    Rewards.php
    Implemented a leaderboard

--- Rewards.php
+++ Rewards.php
@@ -1,1 +1,65 @@
+<style type="text/css">
+	@import url('MainStyle.css');
+</style>
+
 <!DOCTYPE php>
+<?php
+	//connect to server
+     $conn = new mysqli('localhost', 'root', 'tu3xooGh');
+     if($conn -> connect_error) {
+		die("Connection failed: " . $conn -> connect_error);
+     }
+	echo "Connected successfully";
+     
+     //user Razorportal MYSQL database
+     $query = "USE razorportal;";
+    if($conn -> query($query) === TRUE)
+		echo "DATABASE ACCESS SUCCESSFUL\n";
+	else
+		echo "ERROR OPENING DATABASE\n" . $conn -> error;
+?>
+<html>
+	<body  link = "white" vlink = "white">
+
+	<ul>
+		<li><a href="profile.php">Home</a></li>
+		<li><a href="Schedule.php">Edit Schedule</a></li>
+		<li><a href="Map.php">Campus Map</a></li>
+		<li><a href="Social.php">Social Wall</a></li>
+		<li><a href="Rewards.php">Rewards</a></li>
+	 </ul> 
+
+	<h1 align="center">Leaderboard</h1>
+	
+	<table align="center" name="leaderboard">
+		 <tr>
+            <th>Rank</th>
+            <th>Username</th>
+            <th>Points</th>
+         </tr>
+         <tr>
+            <?php
+            //Get schedule from RAZORPORTAL sql
+            $getRanks = "SELECT username, points FROM user ORDER BY points DESC LIMIT 25;";
+            $schedule = $conn->query($getRanks);
+
+            $rank = 1;
+            while ($row = $schedule->fetch_array(MYSQLI_ASSOC)) {
+				echo "<tr>";
+				echo "<td>".$rank."</td>";
+				echo "<td>".$row['username']."</td>";
+				echo "<td>".$row['points']."</td>";
+				echo "</tr>";
+
+				++$rank;
+            }
+            ?>
+         </tr> 
+	</table>
+
+	<?php
+		$conn->close();	
+	?>
+
+	</body>
+   </html>
