d54397d2a3b3170498e695c577ce2bdafb87e919
Freeman-UofA <calvin.freeman2@gmail.com>
2015-04-08 22:20:30 -0500

    Aesthetics changed on profile.php
    Profile.php changed aesthetically with bootstrap
    Table needs to be resized

--- profile.php
+++ profile.php
@@ -1,112 +1,97 @@
-<style type="text/css">
-	@import url('MainStyle.css');
-</style>
-
-<?php
-   session_start();
-   
-    //connect to server
-	 $conn = new mysqli('localhost', 'root', 'tu3xooGh');
-	 if($conn -> connect_error) {
-		die("Connection failed: " . $conn -> connect_error);
-	 }
-		echo "Connected successfully";
-	 
-	 //user Razorportal MYSQL database
-	 $query = "USE razorportal;";
-	 if($conn -> query($query) === TRUE)
-		echo "DATABASE ACCESS SUCCESSFUL\n";
-	 else
-		echo "ERROR OPENING DATABASE\n" . $conn -> error;
-   ?>
-   
-<!DOCTYPE php>
+<!DOCTYPE html>
 <html>
-   <head>
-      <style>
-         h1{ 	
-         color: white; 
-         text-align: center;
-         font-size: 700%; 
-         font-family: "Arial Black", Gadget, sans-serif}
-         }
-      </style>
-   </head>
-   <body   link = "white" vlink = "white">
-   
-	 <ul>
-		<li><a href="profile.php">Home</a></li>
-		<li><a href="Schedule.php">Edit Schedule</a></li>
-		<li><a href="Map.php">Campus Map</a></li>
-		<li><a href="Social.php">Social Wall</a></li>
-		<li><a href="Rewards.php">Rewards</a></li>
-	 </ul> 
-	 
-    <?php
-		if($_SERVER["REQUEST_METHOD"] == "POST") {        
-			if (isset($_POST["addclass"])){
-				 //Check for empty fields on class submission
-				 if(empty($_POST["ccode"]) || empty($_POST["cname"]) ||
-					empty($_POST["days"]) || empty($_POST["time"]) || empty($_POST["building"]) || empty($_POST["room"])) {
-					echo "You must fill out entire form to add a new class";
-				 }
-				 else{
-					 //Insert values to mysql database
-					 $insertrow = "INSERT INTO schedule (username, classcode, classname, days, time, building, room)";
-					 $insertrow .= " VALUES (" . "'" . $_SESSION["username"] . "'";
-					 $insertrow .= " , " . "'" . $_POST["ccode"] . "'";
-					 $insertrow .= " , " . "'" . $_POST["cname"] . "'";
-					 $insertrow .= " , " . "'" . $_POST["days"] . "'";
-					 $insertrow .= " , " . "'" . $_POST["time"] . "'";
-					 $insertrow .= " , " . "'" . $_POST["building"] . "'";
-					 $insertrow .= " , " . "'" . $_POST["room"] . "'" . ");";
-					 var_dump($insertrow);
-				 if($conn -> query($insertrow) === TRUE)
-					echo "DATABASE ACCESS SUCCESSFUL\n";
-				 else
-					echo "ERROR INSERTING INTO DATABASE\n" . $conn -> error;
-				 }
-			}
-		 }
-		 //Greeting
-		 echo "<h1> " . $_SESSION["username"] . "'s Profile</h1>"
-    ?>
+
+	<?php
+	session_start();
+
+	//connect to server
+	$conn = new mysqli('localhost', 'root', 'tu3xooGh');
+	if($conn -> connect_error) {
+		die("Connection failed: " . $conn -> connect_error);
+	}
+
+	echo "Connected successfully";
+
+	//user Razorportal MYSQL database
+	$query = "USE razorportal;";
+	if($conn -> query($query) === TRUE)
+		echo "DATABASE ACCESS SUCCESSFUL\n";
+	else
+		echo "ERROR OPENING DATABASE\n" . $conn -> error;
+	?>
 	
-      <table align="center">
-         <tr>
-            <th>Class Code</th>
-            <th>Class Name</th>
-            <th>Days</th>
-            <th>Time</th>
-            <th>Building</th>
-            <th>Room</th>
-         </tr>
-		 
-         <?php
-            //Get schedule from RAZORPORTAL sql
-            $getSchedule = "SELECT classcode, classname, days, time, building, room FROM schedule WHERE username = \"" . $_SESSION["username"] . "\";";
-            $schedule = $conn->query($getSchedule);
-            while ($row = $schedule->fetch_array(MYSQLI_ASSOC)) {
-				echo "<tr>";
-				echo "<td>".$row['classcode']."</td>";
-				echo "<td>".$row['classname']."</td>";
-				echo "<td>".$row['days']."</td>";
-				echo "<td>".$row['time']."</td>";
-				echo "<td>".$row['building']."</td>";
-				echo "<td>".$row['room']."</td>";
-				echo "</tr>";
-            }
-         ?>
-      </table>
-      <br>
+	<head lang="en">
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+		<title> RazorPortal </title>
+	</head>
+   
+	<body>
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="#">RazorPortal</a>
+				</div>
+				<div>
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="profile.php">Home</a></li>
+						<li><a href="Schedule.php">Edit Schedule</a></li>
+						<li><a href="Social.php">Social Wall</a></li> 
+						<li><a href="Rewards.php">Rewards</a></li> 
+				  </ul>
+				</div>
+			</div>
+		</nav>
+		
+		<?php
+			echo "<h1 class='text-center' style='font-size:150px'> " . $_SESSION["username"] . "'s Profile</h1>"
+		?>
+		
+		<div class="container-fluid">
+			<table class="table table-bordered">
+				<thead>
+					<tr>
+						<th>Class Code</th>
+						<th>Class Name</th>
+						<th>Days</th>
+						<th>Time</th>
+						<th>Building</th>
+						<th>Room</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+						//Get schedule from RAZORPORTAL sql
+						$getSchedule = "SELECT classcode, classname, days, time, building, room FROM schedule WHERE username = \"" . $_SESSION["username"] . "\";";
+						$schedule = $conn->query($getSchedule);
+						while ($row = $schedule->fetch_array(MYSQLI_ASSOC)) {
+							echo "<tr>";
+							echo "<td>".$row['classcode']."</td>";
+							echo "<td>".$row['classname']."</td>";
+							echo "<td>".$row['days']."</td>";
+							echo "<td>".$row['time']."</td>";
+							echo "<td>".$row['building']."</td>";
+							echo "<td>".$row['room']."</td>";
+							echo "</tr>";
+						}
+					?>
+				</tbody>
+			</table>
+		</div>
+		<br>
 	  
-	   <form method="link" action="Login.php">
-	    	<input style="margin-top:15px; margin-left:150px;" class="myButton" type="submit"  value="Log Out">
-      </form>
-	  
-   </body>
-<?php
-	$conn->close();	
-?>
+	  <form method="link" action="Login.php">
+			<div class="text-center">
+				<input class="btn btn-default" type="submit" value="Log Out">
+			</div>
+		</form>
+  </body>
+	
+	<?php
+		$conn->close();	
+	?>
+
 </html>
 
